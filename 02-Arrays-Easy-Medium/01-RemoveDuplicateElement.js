1c7ab087da99cb3bbc0a4ccdb65d15bd67fb80fc
Mohd Adnan <mohdadnan3508@gmail.com>
2025-10-03 11:56:41 +0530

    Remove Duplicate Element in an Array Leetcode 26

---
+++ 02-Arrays-Easy-Medium/01-RemoveDuplicateElement.js
@@ -0,0 +1,19 @@
+// Remove Duplicate From Sorted Array Leetcode no 26
+
+function removeDuplicatesElement(nums) {
+    let x = 0;
+
+    for(let i=0; i<nums.length; i++) {
+        if(nums[i] > nums[x]) {
+            x = x + 1;
+            nums[x] = nums[i];
+        }
+    }
+
+    return x + 1;
+};
+
+let arr = [0,0,1,1,1,2,2,3,3,4];
+let result = removeDuplicatesElement(arr);
+
+console.log(arr.slice(0, result));  // Output => [ 0, 1, 2, 3, 4 ] 
