69c85d10d76644976b795fc583035cefbd97ba68
Mohd Adnan <mohdadnan3508@gmail.com>
2025-10-05 16:52:33 +0530

    Missing Number Leetcode => 268

---
+++ 02-Arrays-Easy-Medium/07-MissingNumber.js
@@ -0,0 +1,18 @@
+// Missing Number Leetcode => 268
+
+function missingNumber(nums) {
+  let n = nums.length;
+  let totalSum = n * (n + 1) / 2;
+  let sum = 0;
+
+  for(let i=0; i<n; i++) {
+    sum += nums[i];
+  }
+
+  return totalSum - sum;
+};
+
+let nums = [9,6,4,2,3,5,7,0,1]
+let result = missingNumber(nums);
+
+console.log(result);
