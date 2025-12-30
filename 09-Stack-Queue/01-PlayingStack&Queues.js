0a788f90bd7c2747365dfc13b41d852d3d7dd129
Mohd Adnan <mohdadnan3508@gmail.com>
2025-12-30 16:43:58 +0530

    Playing Stack & Queue

---
+++ 09-Stack-Queue/01-PlayingStack&Queues.js
@@ -0,0 +1,44 @@
+// // Stacks 
+
+// let stack = [];
+
+// // add a value in stack on front
+
+// stack.push(2);
+// stack.push(4);
+// stack.push(6);
+
+// console.log(stack);
+
+// // remove a value in stack in last
+
+// stack.pop();
+// stack.push(7);
+// stack.push(2);
+// stack.push(8);
+// stack.push(9);
+
+// console.log(stack);
+
+
+// let top = stack[stack.length -1]; 
+// console.log(top);
+
+// Queues
+
+let queue = [];
+
+// add a enqueue 
+
+queue.push(2);
+queue.push(3);
+queue.push(4);
+queue.push(5);
+
+console.log(queue);
+
+// remove from dequeue
+
+queue.shift()
+
+console.log(queue);
